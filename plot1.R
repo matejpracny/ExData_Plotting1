3a76094c632200e0496e7eda39f6dae5e43048cf
matejpracny <matejpracny@gmail.com>
2014-10-11 15:22:12 +0200

    Updated domments

--- plot1.R
+++ plot1.R
@@ -1,13 +1,13 @@
 ## Set local time and language:
 Sys.setlocale("LC_TIME", "English")
 
-## Reading the dataset:
+## Read the dataset:
 d <- read.table("household_power_consumption.txt", sep=";", header=T, colClasses=c("character", "character", rep("numeric", 7)), na.strings="?")
 
-## Converting the date column to Date class:
+## Convert the date column to Date class:
 d$Date <- as.Date(d$Date, format="%d/%m/%Y")
 
-## Subsetting the data.frame to only include the desired dates:
+## Subset the data.frame to include only the desired dates:
 d <- d[d$Date >= as.Date("2007-02-01") & d$Date<=as.Date("2007-02-02"),]
 
 ## Start a PNG device and plot the histogram of global_active_power:
--- plot3.R
+++ plot3.R
@@ -1,19 +1,19 @@
 ## Set local time and language:
 Sys.setlocale("LC_TIME", "English")
 
-## Reading the dataset:
+## Read the dataset:
 d <- read.table("household_power_consumption.txt", sep=";", header=T, colClasses=c("character", "character", rep("numeric", 7)), na.strings="?")
 
-## Converting the date column to Date class:
+## Convert the date column to Date class:
 d$Date <- as.Date(d$Date, format="%d/%m/%Y")
 
-## Subsetting the data.frame to only include the desired dates:
+## Subset the data.frame to include only the desired dates:
 d <- d[d$Date >= as.Date("2007-02-01") & d$Date<=as.Date("2007-02-02"),]
 
 ## Convert Date and Time columns to POSIXlt class:
 d$Date <- as.POSIXlt(paste(as.Date(d$Date, format="%d/%m/%Y"), d$Time, sep=" "))
 
-## Start a PNG device and plot all 3 submeterings vs. time:
+## Start a PNG device and plot submeterings vs. time:
 png("plot3.png", width=480, height=480)
 par(mar=c(4.7, 4.7, 0.7, 0.7))
 
